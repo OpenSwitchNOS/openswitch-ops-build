48a9f6ddcee131cbfef52d8aefa1e241c50a1be3
Walinton Cambronero <walinton.cambronero@hpe.com>
2016-04-15 17:18:30 -0700

    Make the testenv_run target bind to Docker volume
    Tags: chg, dev, wip
    
    With the current code coverage implementation, the way to retrieve
    the coverage files from the Docker container was to bind the local
    directory with the Docker container using Docker volumes.
    This permits for the process in the container to read the gcno files
    and then to write the gcda on process exit.
    
    This change uses the binds argument that the Modular framework
    reads from a json file and parses into -v (volume) arguments
    to Docker when starting the container.
    The change also configures the legacy test framework to achieve
    the same result by exporting the variable it expects.
    
    OpenSwitch already has support for UT code coverage report, that
    recipe has been updated to output a message indicating what type
    of report is being generated.
    
    Change-Id: I50bbee5a5b23023e40cfc6ef6b372a880ff70001
    Signed-off-by: Walinton Cambronero <walinton.cambronero@hpe.com>

--- tools/bin/generate_coverage_report.sh
+++ tools/bin/generate_coverage_report.sh
@@ -30,13 +30,11 @@ COV_CONF_FILE="src/$COV_MODULE/ft_ct_coverage"        # Configuration file. If i
                                                       # example: http://git.openswitch.net/cgit/openswitch/ops-lacpd/tree/ft_ct_coverage
 COV_MINIMUM=-1                                        # Minimum default of coverage. -1 as we want all runs to pass
 COV_REPORT_DIR="coverage"                             # Location where the coverage report data will be placed
-COV_DATA_DIR="src/$COV_MODULE/"                       # Repo directory where coverage data will be stored (gcda && gcno)
+COV_DATA_DIR="src/"                                   # Repo directory where coverage data will be stored (gcda && gcno)
 
 # If the COV_CONF_FILE does not exist, do not continue, i.e no coverage enabled at the repo.
 if [ -f $COV_CONF_FILE ]
 then
-  # Makes the build system compile repos added to the devenv with gcov flags
-  touch build/devenv-coverage-enable
   # Compile the moduler under test, which causes the gcno files to be generated.
   # The gcno files are needed prior to running the tests so that a baseline can be created
   make $COV_MODULE-build
@@ -106,7 +104,7 @@ then
   echo "The configured coverage threshold is: $COV_MINIMUM"
   echo "The configured FT/CT code coverage exclude pattern is: $COV_EXCLUDE_PATTERN"
   echo "The actual reported coverage is: $COV_REPORTED"
-  echo "The location of the report is: $COV_REPORT_DIR/html"
+  echo "The location of the FT/CT code coverage report is: $COV_REPORT_DIR/html"
   if [ "$COV_MINIMUM" -gt "$COV_REPORTED" ]
   then
     echo "The code does not have enough coverage"
