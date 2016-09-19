b757889772a600e02614a9241d6ac6ec95bd2a26
Ashwin Ramani <ashwin.ramani@hpe.com>
2016-09-19 16:25:31 -0700

    chg: dev: Fix for hang seen while reboot
    Change-Id: If030f9af5ddd38bfaec3d3d96e98f7db55287edc
    Signed-off-by: Ashwin Ramani <ashwin.ramani@hpe.com>

--- yocto/openswitch/meta-distro-openswitch/recipes-ops/platform/ops-reboot/switch_reboot.sh
+++ yocto/openswitch/meta-distro-openswitch/recipes-ops/platform/ops-reboot/switch_reboot.sh
@@ -4,4 +4,7 @@ while [[ $(ps -ef | grep systemd-coredump | grep $1 | grep -v grep) ]]
 do
     sleep 5
 done
+systemctl stop switchd
+systemctl stop ops-bgpd
+systemctl stop ovsdb-server
 /sbin/reboot -f
